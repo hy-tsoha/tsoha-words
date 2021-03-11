bed905a787b92b3748fc0cab94190bb925f9fa02
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2021-03-11 11:49:05 +0200

    Create first version

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return "test"
