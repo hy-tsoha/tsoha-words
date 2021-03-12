a689c7aae5951186e968d605eaf83e08a1d33924
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2021-03-12 19:45:41 +0200

    Improve layout

--- stats.py
+++ stats.py
@@ -5,11 +5,11 @@ def get_deck_stats(deck_id, user_id):
     return db.session.execute(sql, {"deck_id":deck_id, "user_id":user_id}).fetchone()
 
 def get_full_stats(user_id):
-    sql = "SELECT id, name FROM decks WHERE creator_id=:user_id"
+    sql = "SELECT id, name FROM decks WHERE creator_id=:user_id ORDER BY name"
     decks = db.session.execute(sql, {"user_id": user_id}).fetchall()
     data = []
     for deck in decks:
-        sql = "SELECT u.name, COUNT(*), COALESCE(SUM(a.result),0) FROM answers a, cards c, users u WHERE c.deck_id=:deck_id AND a.card_id=c.id AND u.id=a.user_id GROUP BY u.id, u.name"
+        sql = "SELECT u.name, COUNT(*), COALESCE(SUM(a.result),0) FROM answers a, cards c, users u WHERE c.deck_id=:deck_id AND a.card_id=c.id AND u.id=a.user_id GROUP BY u.id, u.name ORDER BY u.name"
         results = db.session.execute(sql, {"deck_id": deck[0]}).fetchall()
         data.append((deck[1], results))
     return data
